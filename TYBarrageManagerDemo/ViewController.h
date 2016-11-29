f15ed24ec7c311025050ea825966f6d794bbac98
tanyang <122074809@qq.com>
2016-11-29 14:39:05 +0800

    Initial Commit

---
+++ TYBarrageManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYBarrageManagerDemo
+//
+//  Created by tany on 16/11/29.
+//  Copyright © 2016年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
